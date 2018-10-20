1ebfc2f9d228811b45955e1d7ddd1f61407a3809
Jason Forbes <jason.g.forbes@gmail.com>
2018-10-20 11:17:32 -0230

    Use CssNormalize to normalize padding/margin for entire app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 
 const App = () => (
   <React.Fragment>
+    <CssBaseline />
     <Router>
       <Route exact path="/" component={Home} />
     </Router>
