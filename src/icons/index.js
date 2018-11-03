9cb66ea703c3a3b4882f6dc917bddf34ffddfc71
Jason Forbes <jason.g.forbes@gmail.com>
2018-11-03 13:21:03 -0230

    Added image assets for the Header component

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,6 +1,9 @@
+import EmailInner from './emailInner';
 import EmailRound from './emailRound';
 import Home from './home';
+import InstagramInner from './instagramInner';
 import InstagramRound from './instagramRound';
+import TwitterInner from './twitterInner';
 import TwitterRound from './twitterRound';
 
-export { EmailRound, Home, InstagramRound, TwitterRound };
+export { EmailInner, EmailRound, Home, InstagramInner, InstagramRound, TwitterInner, TwitterRound };
