02a1b3ab7dcb8f31276f6a3b9273e54ee9c4e0dd
Jason Forbes <jason.g.forbes@gmail.com>
2018-11-04 11:37:00 -0330

    Added cancel and menu icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,21 @@
+import Cancel from './cancel';
 import EmailInner from './emailInner';
 import EmailRound from './emailRound';
 import Home from './home';
 import InstagramInner from './instagramInner';
 import InstagramRound from './instagramRound';
+import Menu from './menu';
 import TwitterInner from './twitterInner';
 import TwitterRound from './twitterRound';
 
-export { EmailInner, EmailRound, Home, InstagramInner, InstagramRound, TwitterInner, TwitterRound };
+export {
+  Cancel,
+  EmailInner,
+  EmailRound,
+  Home,
+  InstagramInner,
+  InstagramRound,
+  Menu,
+  TwitterInner,
+  TwitterRound,
+};
