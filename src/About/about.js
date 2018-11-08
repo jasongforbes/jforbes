5d7cc13f3c8360675bcfae07a4c7c92789fcf8e7
Jason Forbes <jason.g.forbes@gmail.com>
2018-11-07 20:39:12 -0330

    Moved content styling to individual components

--- src/About/about.js
+++ src/About/about.js
@@ -16,64 +16,72 @@ const styles = theme => ({
     paddingTop: '40.598690364%',
     margin: '40px 0px',
   },
+  content: {
+    margin: '80px 0px',
+    [theme.breakpoints.down('sm')]: {
+      margin: '40px 0px',
+    },
+  },
   listItem: theme.typography.list,
 });
 
 const About = ({ classes }) => (
-  <Grid container spacing={0}>
-    <Hidden mdUp>
-      <Grid item xs={1} />
-    </Hidden>
-    <Grid item xs={10} md={12}>
-      <Typography variant="h3">About</Typography>
-      <Typography variant="body1">
-        I am a software engineer from St. John&#39;s, Newfoundland. I have a Master&#39;s in Applied
-        Sciences, with a focus in power-converter simulation and efficient computation of systems. I
-        currently work as a Senior Software Engineer for{' '}
-        <a href="http://krakenrobotics.com/">Kraken Robotics</a>.
-      </Typography>
-      <Typography variant="body1">Since graduation, my focus has been in:</Typography>
-      <ul>
-        <li>
-          <Typography className={classes.listItem}>real-time systems</Typography>
-        </li>
-        <li>
-          <Typography className={classes.listItem}>high-performance computing</Typography>
-        </li>
-        <li>
-          <Typography className={classes.listItem}>numerical lineal algebra</Typography>
-        </li>
-      </ul>
-      <Typography variant="body1">More recently, I have focused my studies in:</Typography>
-      <ul>
-        <li>
-          <Typography className={classes.listItem}>full-stack development</Typography>
-        </li>
-        <li>
-          <Typography className={classes.listItem}>machine learning</Typography>
-        </li>
-        <li>
-          <Typography className={classes.listItem}>distributed systems</Typography>
-        </li>
-      </ul>
-      <Typography variant="body1">
-        I am always interested in hearing about other people&#39;s work and ongoing projects, and am
-        happy to consult or provide advice where I can. Feel free to reach me at{' '}
-        <a href="mailto:jason@jforbes.io">jason@jforbes.io</a> or on{' '}
-        <a href="https://twitter.com/JasonForbes">Twitter</a>.
-      </Typography>
+  <div className={classes.content}>
+    <Grid container spacing={0}>
+      <Hidden mdUp>
+        <Grid item xs={1} />
+      </Hidden>
+      <Grid item xs={10} md={12}>
+        <Typography variant="h3">About</Typography>
+        <Typography variant="body1">
+          I am a software engineer from St. John&#39;s, Newfoundland. I have a Master&#39;s in
+          Applied Sciences, with a focus in power-converter simulation and efficient computation of
+          systems. I currently work as a Senior Software Engineer for{' '}
+          <a href="http://krakenrobotics.com/">Kraken Robotics</a>.
+        </Typography>
+        <Typography variant="body1">Since graduation, my focus has been in:</Typography>
+        <ul>
+          <li>
+            <Typography className={classes.listItem}>real-time systems</Typography>
+          </li>
+          <li>
+            <Typography className={classes.listItem}>high-performance computing</Typography>
+          </li>
+          <li>
+            <Typography className={classes.listItem}>numerical lineal algebra</Typography>
+          </li>
+        </ul>
+        <Typography variant="body1">More recently, I have focused my studies in:</Typography>
+        <ul>
+          <li>
+            <Typography className={classes.listItem}>full-stack development</Typography>
+          </li>
+          <li>
+            <Typography className={classes.listItem}>machine learning</Typography>
+          </li>
+          <li>
+            <Typography className={classes.listItem}>distributed systems</Typography>
+          </li>
+        </ul>
+        <Typography variant="body1">
+          I am always interested in hearing about other people&#39;s work and ongoing projects, and
+          am happy to consult or provide advice where I can. Feel free to reach me at{' '}
+          <a href="mailto:jason@jforbes.io">jason@jforbes.io</a> or on{' '}
+          <a href="https://twitter.com/JasonForbes">Twitter</a>.
+        </Typography>
+      </Grid>
+      <Hidden mdUp>
+        <Grid item xs={1} />
+      </Hidden>
+      <Grid item xs={12}>
+        <div
+          className={classes.aboutImage}
+          style={{ backgroundImage: `url(${about})` }}
+          alt="Jason in Bonavista"
+        />
+      </Grid>
     </Grid>
-    <Hidden mdUp>
-      <Grid item xs={1} />
-    </Hidden>
-    <Grid item xs={12}>
-      <div
-        className={classes.aboutImage}
-        style={{ backgroundImage: `url(${about})` }}
-        alt="Jason in Bonavista"
-      />
-    </Grid>
-  </Grid>
+  </div>
 );
 
 About.propTypes = {
--- src/Page/page.js
+++ src/Page/page.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
 import { Route } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
 import About from '../About';
 import Header from '../Header';
 import MobileHeader from '../MobileHeader';
@@ -11,16 +9,7 @@ import Posts from '../Posts';
 import Projects from '../Projects';
 import Sidebar from '../Sidebar';
 
-const styles = theme => ({
-  content: {
-    margin: '80px 0px',
-    [theme.breakpoints.down('sm')]: {
-      margin: '40px 0px',
-    },
-  },
-});
-
-const Page = ({ classes }) => (
+const Page = () => (
   <Grid container spacing={0}>
     <Hidden smDown>
       <Grid item md={3}>
@@ -41,11 +30,9 @@ const Page = ({ classes }) => (
       <Grid item xs={1} xl={2} />
     </Hidden>
     <Grid item xs={12} md={7} xl={5}>
-      <div className={classes.content}>
         <Route exact path="/" component={Posts} />
         <Route path="/about" component={About} />
         <Route path="/projects" component={Projects} />
-      </div>
     </Grid>
     <Hidden smDown>
       <Grid item xs={1} xl={2} />
@@ -53,8 +40,4 @@ const Page = ({ classes }) => (
   </Grid>
 );
 
-Page.propTypes = {
-  classes: PropTypes.objectOf(PropTypes.string).isRequired,
-};
-
-export default withStyles(styles)(Page);
+export default Page;
