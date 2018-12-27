9e8409b434171cca958178be2ecb5c0330a44d58
Jason Forbes <jason.g.forbes@gmail.com>
2018-12-27 11:36:16 -0330

    Replaced mathjax with katex
    Fixed math equations that were too large

--- src/Posts/LinearFitDiscontinuities/linearFitDiscontinuities.js
+++ src/Posts/LinearFitDiscontinuities/linearFitDiscontinuities.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MathJax from 'react-mathjax';
+import { InlineMath, BlockMath } from 'react-katex';
 import { Helmet } from 'react-helmet';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
@@ -20,6 +20,8 @@ const styles = theme => ({
   },
   math: {
     ...theme.typography.body1,
+    overflowX: 'auto',
+    marginBottom: '0',
   },
   quote: {
     padding: '18px 27px',
@@ -30,7 +32,7 @@ const styles = theme => ({
       ...theme.typography.body1,
       color: '#5B626B',
     },
-    '& math': {
+    '& .katex-display': {
       ...theme.typography.body1,
       color: '#5B626B',
     },
@@ -40,10 +42,16 @@ const styles = theme => ({
 const Post = ({ classes, match }) => {
   const { date, title, summary } = postData(match.url)[0];
   return (
-    <MathJax.Provider>
+    <React.Fragment>
       <Helmet>
         <title>{`${title} - Jason Forbes`}</title>
         <meta name="description" content={summary} />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.10.0/dist/katex.min.css"
+          integrity="sha384-9eLZqc9ds8eNjO3TmqPeYcDj8n+Qfa4nuSiGYa6DjLNcv9BtN69ZIulL9+8CqC9Y"
+          crossOrigin="anonymous"
+        />
       </Helmet>
       <React.Fragment>
         <Typography variant="h3">{title}</Typography>
@@ -89,7 +97,7 @@ const Post = ({ classes, match }) => {
           We will use this problem to explore the basics of the Fourier Transform. Find the code for
           all plots{' '}
           <a href="https://github.com/jasongforbes/jforbes.io/tree/master/src/Posts/LinearFitDiscontinuities">
-            here
+            on Github
           </a>
           . Although this post will keep the wall-clock metaphor throughout, the solution applies to
           many problems. It is suitable whenever you are sampling a function with linear
@@ -149,33 +157,35 @@ const Post = ({ classes, match }) => {
         <Typography variant="body1">
           In the above problem, we can model the variable-speed clock as a linear equation.
         </Typography>
-        <MathJax.Node className={classes.math} formula="y = Ax" />
+        <div className={classes.math}>
+          <BlockMath math="y = Ax" />{' '}
+        </div>
         <Typography variant="body1">
-          Keeping with the example, <MathJax.Node inline formula="A" /> is a measure of the
-          malfunctioning clock speed as compared to a working clock.{' '}
-          <MathJax.Node inline formula="x" />, the input, is the actual time.{' '}
-          <MathJax.Node inline formula="y" /> is the time shown by the clock. It should be pretty
-          easy to see that if <MathJax.Node inline formula="A = 1" />, then{' '}
-          <MathJax.Node inline formula="y = x" />, and the clock runs neither fast nor slow.
+          Keeping with the example, <InlineMath math="A" /> is a measure of the malfunctioning clock
+          speed as compared to a working clock. <InlineMath math="x" />, the input, is the actual
+          time. <InlineMath math="y" /> is the time shown by the clock. It should be pretty easy to
+          see that if <InlineMath math="A = 1" />, then <InlineMath math="y = x" />, and the clock
+          runs neither fast nor slow.
         </Typography>
         <Typography variant="body1">
-          A random variable <MathJax.Node inline formula={'\\tilde \\mu(x)'} /> models the noise in
-          the output due to errors in the observation process. In this post, random variables are
-          shown with the <MathJax.Node inline formula={'\\tilde \\cdot'} /> symbol.
+          A random variable <InlineMath math={'\\tilde \\mu(x)'} /> models the noise in the output
+          due to errors in the observation process. In this post, random variables are shown with
+          the <InlineMath math={'\\tilde \\cdot'} /> symbol.
         </Typography>
-        <MathJax.Node className={classes.math} formula={'\\tilde y = Ax + \\tilde \\mu(x)'} />
+        <div className={classes.math}>
+          <BlockMath math={'\\tilde y = Ax + \\tilde \\mu(x)'} />{' '}
+        </div>
         <Typography variant="body1">
           Lastly, we have to model the discontinuities that are the impetus for this entire blog
           post. In its simplest form, this discontinuity is due to the estimator &quot;rolling
           over&quot;. In the broken clock example, this is the clock changing from 11:59 to 0:00 at
           the stroke of midnight. The modulo operator captures this effect.
         </Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={'\\tilde y = Ax + \\tilde \\mu(x) \\ \\ mod \\ \\ b'}
-        />
+        <div className={classes.math}>
+          <BlockMath math={'\\tilde y = Ax + \\tilde \\mu(x) \\ \\ mod \\ \\ b'} />{' '}
+        </div>
         <Typography variant="body1">
-          Our goal now is to find an estimate for <MathJax.Node inline formula="A" />.
+          Our goal now is to find an estimate for <InlineMath math="A" />.
         </Typography>
         <Typography variant="h4">A Brief Detour – The Fourier Transform</Typography>
         <Typography variant="body1">
@@ -191,54 +201,59 @@ const Post = ({ classes, match }) => {
         </Typography>
         <Typography variant="body1">
           More precisely, a real harmonic is a sinusoidal function with an amplitude{' '}
-          <MathJax.Node inline formula="A" /> and phase <MathJax.Node inline formula={'\\phi'} />.
-          The qualifier &quot;real&quot; here means &quot;not imaginary&quot;. In this context,
-          amplitude defines the height of the wave and phase defines the &quot;starting-point&quot;.
-          Each harmonic has a constant frequency <MathJax.Node inline formula={'\\omega'} /> related
-          to its rate of change.
+          <InlineMath math="A" /> and phase <InlineMath math={'\\phi'} />. The qualifier
+          &quot;real&quot; here means &quot;not imaginary&quot;. In this context, amplitude defines
+          the height of the wave and phase defines the &quot;starting-point&quot;. Each harmonic has
+          a constant frequency <InlineMath math={'\\omega'} /> related to its rate of change.
         </Typography>
-        <MathJax.Node className={classes.math} formula={'A \\sin(\\omega x + \\phi)'} />
+        <div className={classes.math}>
+          <BlockMath math={'A \\sin(\\omega x + \\phi)'} />{' '}
+        </div>
         <Typography variant="body1">
           For simplicity, the Fourier Transform typically represents harmonics as a complex
           exponential. Using{' '}
           <a href="https://en.wikipedia.org/wiki/Euler%27s_formula">Euler&apos;s Formula</a>:
         </Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={'e^{i \\phi} = \\cos(\\phi) + i \\sin(\\phi)'}
-        />
+        <div className={classes.math}>
+          <BlockMath math={'e^{i \\phi} = \\cos(\\phi) + i \\sin(\\phi)'} />{' '}
+        </div>
         <Typography variant="body1">
-          The output of the Fourier Transform, <MathJax.Node inline formula={'F(\\omega)'} />,
-          describes the amplitude and phase of the harmonic at frequency{' '}
-          <MathJax.Node inline formula={'\\omega'} />. The amplitude and phase are also represented
-          as a complex number. Under certain circumstances this can completely define a real
-          harmonic:
+          The output of the Fourier Transform, <InlineMath math={'F(\\omega)'} />, describes the
+          amplitude and phase of the harmonic at frequency <InlineMath math={'\\omega'} />. The
+          amplitude and phase are also represented as a complex number. Under certain circumstances
+          this can completely define a real harmonic:
         </Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={'A \\sin(\\omega x + \\phi) = F(\\omega)e^{i \\omega x}'}
-        />
+        <div className={classes.math}>
+          <BlockMath math={'A \\sin(\\omega x + \\phi) = F(\\omega)e^{i \\omega x}'} />{' '}
+        </div>
         <Typography variant="body1">
           I will use the complex exponential treatment here, as it simplifies the math. The
           introduction of complex numbers does tend to cause confusion though. For this reason, I
           want you to think of a complex exponential as the real harmonic described above.
         </Typography>
         <Typography variant="body1">Formally, the Inverse Fourier Transform is:</Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={'f(x) = \\int_{- \\infty}^{\\infty} F(\\omega) e^{i\\omega x} d\\omega'}
-        />
+        <div className={classes.math}>
+          <BlockMath
+            math={'f(x) = \\int_{- \\infty}^{\\infty} F(\\omega) e^{i\\omega x} d\\omega'}
+          />{' '}
+        </div>
         <Typography variant="body1">
           To finish this introduction to Fourier Transforms, I will reiterate the definition of the
           Fourier Transform.
         </Typography>
         <div className={classes.quote}>
           <p>The Fourier Transform changes a problem by modeling a function</p>
-          <MathJax.Node className={classes.math} formula="f(x)" />
+          <div className={classes.math}>
+            <BlockMath math="f(x)" />{' '}
+          </div>
           <p variant="body1">as an infinite sum</p>
-          <MathJax.Node className={classes.math} formula={'\\int_{- \\infty}^{\\infty} d\\omega'} />
+          <div className={classes.math}>
+            <BlockMath math={'\\int_{- \\infty}^{\\infty} d\\omega'} />{' '}
+          </div>
           <p variant="body1">of its harmonics</p>
-          <MathJax.Node className={classes.math} formula={'F(\\omega) e^{i\\omega x}'} />
+          <div className={classes.math}>
+            <BlockMath math={'F(\\omega) e^{i\\omega x}'} />{' '}
+          </div>
         </div>
         <Typography variant="h4">The Solution to our Original Query</Typography>
         <Typography variant="body1">
@@ -246,64 +261,65 @@ const Post = ({ classes, match }) => {
           problem. I will use that approach here.
         </Typography>
         <Typography variant="body1">
-          Recall the model for the sampled data <MathJax.Node inline formula={'(\\tilde y, x)'} />.
+          Recall the model for the sampled data <InlineMath math={'(\\tilde y, x)'} />.
         </Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={'\\tilde y = Ax + \\tilde \\mu(x) \\ \\ mod \\ \\ b'}
-        />
+        <div className={classes.math}>
+          <BlockMath math={'\\tilde y = Ax + \\tilde \\mu(x) \\ \\ mod \\ \\ b'} />{' '}
+        </div>
         <Typography variant="body1">
-          For the example of the clock, <MathJax.Node inline formula={'\\tilde y'} /> is the
-          observed time, <MathJax.Node inline formula="x" /> is the actual time, and{' '}
-          <MathJax.Node inline formula="b = 12" /> is the discontinuity point. One possible
-          transformation is to change the discontinuity point by scaling the model.
+          For the example of the clock, <InlineMath math={'\\tilde y'} /> is the observed time,{' '}
+          <InlineMath math="x" /> is the actual time, and <InlineMath math="b = 12" /> is the
+          discontinuity point. One possible transformation is to change the discontinuity point by
+          scaling the model.
         </Typography>
         <Typography variant="body1">
-          For instance, we can introduce a scaling factor <MathJax.Node inline formula="c" />:
+          For instance, we can introduce a scaling factor <InlineMath math="c" />:
         </Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={'c \\tilde y = c (Ax + \\tilde \\mu(x)) \\ \\ mod \\ \\ c b'}
-        />
+        <div className={classes.math}>
+          <BlockMath math={'c \\tilde y = c (Ax + \\tilde \\mu(x)) \\ \\ mod \\ \\ c b'} />{' '}
+        </div>
         <Typography variant="body1">
-          If we can determine the new speed factor <MathJax.Node inline formula="cA" />, we can
-          recover the desired solution <MathJax.Node inline formula="A" /> by undoing the scaling.
+          If we can determine the new speed factor <InlineMath math="cA" />, we can recover the
+          desired solution <InlineMath math="A" /> by undoing the scaling.
         </Typography>
         <Typography variant="body1">
           Next, we can transform the problem by introducing a complex exponential.
         </Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={'e^{i c \\tilde y} = e^{i c (Ax + \\tilde \\mu(x)) \\ \\ mod \\ \\ c b}'}
-        />
+        <div className={classes.math}>
+          <BlockMath
+            math={'e^{i c \\tilde y} = e^{i c (Ax + \\tilde \\mu(x)) \\ \\ mod \\ \\ c b}'}
+          />
+        </div>
         <Typography variant="body1">
           The complex exponential is a periodic function with period of{' '}
-          <MathJax.Node inline formula={'2 \\pi'} />. That means it &quot;wraps&quot; around every{' '}
-          <MathJax.Node inline formula={'2 \\pi'} /> much like the wall-clock does ever 12 hours
+          <InlineMath math={'2 \\pi'} />. That means it &quot;wraps&quot; around every{' '}
+          <InlineMath math={'2 \\pi'} /> much like the wall-clock does ever 12 hours
         </Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={
-            '\\begin{aligned}\
+        <div className={classes.math}>
+          <BlockMath
+            math={
+              '\\begin{aligned}\
           e^{i \\phi} &= e^{i (\\phi + 2\\pi)} \\\\\
           &= e^{i\\phi \\ \\ mod \\ \\ 2 \\pi} \\\\\
           \\end{aligned}'
-          }
-        />
+            }
+          />
+        </div>
         <Typography variant="body1">
           Using this knowledge, note what happens when we choose{' '}
-          <MathJax.Node inline formula={'c = 2 \\pi / b'} />.
+          <InlineMath math={'c = 2 \\pi / b'} />.
         </Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={
-            '\\begin{aligned}\
+        <div className={classes.math}>
+          <BlockMath
+            math={
+              '\\begin{aligned}\
             e^{i 2 \\pi \\tilde y / b} &= e^{i 2 \\pi (Ax + \\tilde \\mu(x)) /b  \\ \\ mod \\ \\ 2\\pi }\\\\\
             &= e^{i 2 \\pi (Ax + \\tilde \\mu(x)) /b}\\\\\
             &= e^{i 2 \\pi Ax / b }e^{i 2 \\pi \\tilde \\mu(x) b }\\\\\
           \\end{aligned}'
-          }
-        />
+            }
+          />
+        </div>
         <Typography variant="body1">
           Next, the Fourier Transform transforms the problem into the frequency domain. Instead of
           deriving the Fourier Transform, see this table of{' '}
@@ -312,32 +328,34 @@ const Post = ({ classes, match }) => {
           </a>
           .
         </Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={'F(e^{i \\phi x}) = \\sqrt {2\\pi} \\ \\ \\delta (\\omega - \\phi)'}
-        />
+        <div className={classes.math}>
+          <BlockMath math={'F(e^{i \\phi x}) = \\sqrt {2\\pi} \\ \\ \\delta (\\omega - \\phi)'} />{' '}
+        </div>
         <Typography variant="body1">
           This says that in the frequency domain, a complex exponential will be a &quot;spike&quot;
-          or impulse at frequency <MathJax.Node inline formula={'\\phi'} />. In our case:
+          or impulse at frequency <InlineMath math={'\\phi'} />. In our case:
         </Typography>
-        <MathJax.Node className={classes.math} formula={'\\phi = 2\\pi A / b'} />
+        <div className={classes.math}>
+          <BlockMath math={'\\phi = 2\\pi A / b'} />{' '}
+        </div>
         <Typography variant="body1">
           What about the noise in our observations? The noise causes a frequency shift in the
           output. If the noise has a biased, the accumulation of these frequency shifts causes a
           systematic error. But for noise with zero-mean, these systematic errors will cancel out.
         </Typography>
-        <MathJax.Node
-          className={classes.math}
-          formula={
-            '\\begin{aligned}\
+        <div className={classes.math}>
+          <BlockMath
+            math={
+              '\\begin{aligned}\
             E[F(e^{i \\phi x}e^{i\\tilde \\mu(x))})] &= \\sqrt {2\\pi} \\delta (\\omega - \\phi - E[ \\mu(x)]) \\\\\
             &= \\sqrt {2\\pi} \\delta (\\omega - \\phi) \\\\\
           \\end{aligned}'
-          }
-        />
+            }
+          />{' '}
+        </div>
         <Typography variant="body1">
           It should then be trivial to determine the location of the spike, and from it recover the
-          value of <MathJax.Node inline formula="A" />.
+          value of <InlineMath math="A" />.
         </Typography>
         <Typography variant="h4">A Wild Algorithm Appears</Typography>
         <Typography variant="body1">
@@ -351,7 +369,7 @@ const Post = ({ classes, match }) => {
         </Highlight>
         <Typography variant="body1">
           Next, we want to transform the data into an appropriate form. This includes applying a
-          scaling factor <MathJax.Node inline formula="c" /> and introducing a complex harmonic.
+          scaling factor <InlineMath math="c" /> and introducing a complex harmonic.
         </Typography>
         <Highlight language="javascript">
           const c = tf.scalar((2 * Math.PI) / rollover);{'\n'}
@@ -365,9 +383,9 @@ const Post = ({ classes, match }) => {
         <Typography variant="body1">
           By design, the sample data is a harmonic at a frequency which corresponds to the
           clock-speed. The FFT will contain a single spike at the frequency of this harmonic{' '}
-          <MathJax.Node inline formula={'\\phi = 2\\pi A / b'} />. Determine the location of the
-          spike by searching the FFT data. Lastly, convert the frequency back to clock-speed using
-          the inverse scaling factor <MathJax.Node inline formula="1/c" />.
+          <InlineMath math={'\\phi = 2\\pi A / b'} />. Determine the location of the spike by
+          searching the FFT data. Lastly, convert the frequency back to clock-speed using the
+          inverse scaling factor <InlineMath math="1/c" />.
         </Typography>
         <Highlight language="javascript">
           const max = tf.argMax(tf.abs(F));{'\n'}
@@ -393,7 +411,7 @@ const Post = ({ classes, match }) => {
           Transform, linear algebra, and optimization, in future posts.
         </Typography>
       </React.Fragment>
-    </MathJax.Provider>
+    </React.Fragment>
   );
 };
 
