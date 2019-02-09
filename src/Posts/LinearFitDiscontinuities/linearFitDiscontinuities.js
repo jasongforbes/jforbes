32367ae8978eebc768c52bf698dae0c4e5a9e01b
Jason Forbes <jason.g.forbes@gmail.com>
2019-02-09 14:22:18 -0330

    Moved post styles to helper function

--- src/Posts/LinearFitDiscontinuities/linearFitDiscontinuities.js
+++ src/Posts/LinearFitDiscontinuities/linearFitDiscontinuities.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Highlight from '../../Highlight';
 import posts from '../posts.json';
+import styles from '../style';
 import AnimatedPlot from './animatedPlot';
 import CustomizablePlot from './customizablePlot';
 import PhasePlot from './phasePlot';
@@ -13,31 +14,6 @@ import FFTPlot from './fftPlot';
 import 'highlight.js/styles/ocean.css';
 
 const postData = slug => posts.filter(post => post.slug === slug);
-
-const styles = theme => ({
-  indent: {
-    padding: '0px 27px',
-  },
-  math: {
-    ...theme.typography.body1,
-    overflowX: 'auto',
-    marginBottom: '0',
-  },
-  quote: {
-    padding: '18px 27px',
-    margin: '27px',
-    backgroundColor: '#D8E6E7',
-    borderRadius: '8px',
-    '& p': {
-      ...theme.typography.body1,
-      color: '#5B626B',
-    },
-    '& .katex-display': {
-      ...theme.typography.body1,
-      color: '#5B626B',
-    },
-  },
-});
 
 const Post = ({ classes, match }) => {
   const { date, title, summary } = postData(match.url)[0];
